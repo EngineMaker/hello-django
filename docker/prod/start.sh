4379d5eeded62c018f1c030009a3a0c8549b8e48
えむけー <mk@mktoho.org>
2022-11-03 04:08:26 +0900

    もうおしまいだ

--- docker/prod/start.sh
+++ docker/prod/start.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-#python manage.py createsuperuser --no-input --username admin
+python manage.py createsuperuser --no-input --username admin
 
 uvicorn mysite.asgi:application --host 0.0.0.0
