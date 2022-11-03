8ab95d3d6681ec5c5f3ee980a7a24c3c6ce2e878
mktoho <mk@mktoho.org>
2022-11-03 13:16:12 +0900

    setup initial admin user for prod

--- docker/prod/start.sh
+++ docker/prod/start.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input --username admin
+python manage.py shell < docker/dev/create-admin-user.py
 
 uvicorn mysite.asgi:application --host 0.0.0.0
