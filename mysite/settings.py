2df72308154408e89b209028bd54574eaf2457dc
mktoho <mk@mktoho.org>
2022-11-03 15:17:11 +0900

    Add CSRF trusted origin

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 DEBUG = True
-CSRF_TRUSTED_ORIGINS = ['https://django.mktoho.dev/']
+CSRF_TRUSTED_ORIGINS = ['https://django.mktoho.dev']
